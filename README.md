3f8c7f1219d51851f97c583257cfb7c2d4950d66
prabudatasirpi <prabuk@datasirpi.com>
2024-05-13 17:14:22 +0530

    added the new code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# MyAngularMain
+# MyAngular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.3.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-angular-main' title`, () => {
+  it(`should have the 'my_angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular-main');
+    expect(app.title).toEqual('my_angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-main');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my_angular');
   });
 });
